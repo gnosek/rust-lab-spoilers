c7f5c5aad680cd540500e5cb2a219ad0eefd7959
Grzegorz Nosek <root@localdomain.pl>
2020-04-05 12:14:58 +0200

    Update DNS server examples

--- dns/src/bin/tokio-server-future.rs
+++ dns/src/bin/tokio-server-future.rs
@@ -1,137 +1,101 @@
 use dns::build::Serialize;
-use dns::parse::{parse_dns_packet, parse_dns_tcp_packet};
+use dns::parse::parse_dns_packet;
 use dns::utils::respond;
-use futures::{lazy, try_ready};
+use futures::future;
+use futures::sink::SinkExt;
+use futures::stream::StreamExt;
 use std::net::SocketAddr;
 use std::{env, io};
 use tokio::net::{TcpListener, TcpStream, UdpSocket};
-use tokio::prelude::*;
+use tokio_util::codec::LengthDelimitedCodec;
 
-struct UdpServer {
-    socket: UdpSocket,
-    buf: Vec<u8>,
-    to_send: Option<(Vec<u8>, SocketAddr)>,
-}
+async fn udp_server(mut socket: UdpSocket) -> Result<(), io::Error> {
+    let mut buf = vec![0u8; 1024];
+    loop {
+        let (nread, addr) = socket.recv_from(&mut buf).await?;
 
-impl Future for UdpServer {
-    type Item = ();
-    type Error = io::Error;
-
-    fn poll(&mut self) -> Poll<(), io::Error> {
-        loop {
-            if let Some((ref packet, ref peer)) = self.to_send {
-                let nwritten = try_ready!(self.socket.poll_send_to(&packet, &peer));
-                if nwritten != packet.len() {
-                    eprintln!(
-                        "Failed to send whole response, sent {}/{} bytes",
-                        nwritten,
-                        packet.len()
-                    );
-                } else {
-                    println!("Sent response to {}", peer);
-                }
-                self.to_send = None
-            }
-
-            let (nread, addr) = try_ready!(self.socket.poll_recv_from(&mut self.buf));
-            match parse_dns_packet(&self.buf[0..nread]) {
-                Ok((_, packet)) => {
-                    let mut out_buf = Vec::new();
-                    let resp = respond(packet, &addr);
-                    resp.serialize_to(&mut out_buf)?;
-                    self.to_send = Some((out_buf, addr));
-                }
-                Err(e) => {
-                    eprintln!("Malformed DNS query from {:?}: {:?}", addr, e);
-                }
-            }
-        }
-    }
-}
-
-struct TcpServer {
-    socket: TcpStream,
-    buf: Vec<u8>,
-    to_send: Option<(usize, Vec<u8>)>,
-}
-
-impl Future for TcpServer {
-    type Item = ();
-    type Error = io::Error;
-
-    fn poll(&mut self) -> Poll<(), io::Error> {
-        loop {
-            if let Some((ref mut offset, ref mut buf)) = self.to_send {
-                let nwritten = try_ready!(self.socket.poll_write(&buf[*offset..]));
-                *offset += nwritten;
-                let remaining = buf.len() - *offset;
-                if remaining == 0 {
-                    self.to_send = None;
-                } else {
-                    *offset += nwritten;
-                }
-            }
-
-            if self.to_send.is_some() {
+        let packet = match parse_dns_packet(&buf[0..nread]) {
+            Ok((_, packet)) => packet,
+            Err(e) => {
+                eprintln!("Malformed DNS query from {:?}: {:?}", addr, e);
                 continue;
             }
+        };
 
-            let nread = try_ready!(self.socket.poll_read(&mut self.buf));
-            if nread == 0 {
-                return Ok(Async::Ready(()));
+        let mut out_buf = Vec::new();
+        let resp = respond(packet, &addr);
+        resp.serialize_to(&mut out_buf)?;
+        match socket.send_to(&out_buf, addr).await {
+            Ok(nsent) if nsent == out_buf.len() => {
+                println!("Sent response to {}", addr);
             }
-
-            let addr = self.socket.peer_addr()?;
-            match parse_dns_tcp_packet(&self.buf[0..nread]) {
-                Ok((_, packet)) => {
-                    let mut out_buf = Vec::new();
-                    let resp = respond(packet, &addr);
-                    resp.serialize_tcp_to(&mut out_buf)?;
-                    self.to_send = Some((0, out_buf));
-                }
-                Err(e) => {
-                    eprintln!("Malformed DNS query from {:?}: {:?}", addr, e);
-                    return Ok(Async::Ready(()));
-                }
+            Ok(nsent) => {
+                eprintln!(
+                    "Failed to send whole response, sent {}/{} bytes",
+                    nsent,
+                    out_buf.len()
+                );
+            }
+            Err(e) => {
+                eprintln!("Failed to send response to {:?}: {:?}", addr, e);
             }
         }
     }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = env::args().nth(1).unwrap_or("[::]:35353".to_string());
+async fn tcp_handle_connection(socket: TcpStream, addr: SocketAddr) -> Result<(), io::Error> {
+    let mut framed = LengthDelimitedCodec::builder()
+        .length_field_length(2)
+        .new_framed(socket);
+
+    while let Some(frame) = framed.next().await {
+        let packet = match parse_dns_packet(&frame?) {
+            Ok((_, packet)) => packet,
+            Err(e) => {
+                eprintln!("Malformed DNS query from {:?}: {:?}", addr, e);
+                continue;
+            }
+        };
+
+        let mut out_buf = Vec::new();
+        let resp = respond(packet, &addr);
+        resp.serialize_to(&mut out_buf)?;
+        match framed.send(out_buf.into()).await {
+            Ok(()) => {
+                println!("Sent response to {}", addr);
+            }
+            Err(e) => {
+                eprintln!("Failed to send response to {:?}: {:?}", addr, e);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn tcp_server(mut socket: TcpListener) -> Result<(), io::Error> {
+    loop {
+        let (conn_socket, addr) = socket.accept().await?;
+        tokio::spawn(tcp_handle_connection(conn_socket, addr));
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = env::args().nth(1).unwrap_or_else(|| "[::]:35353".to_string());
     let addr = addr.parse::<SocketAddr>()?;
 
-    let udp_socket = UdpSocket::bind(&addr)?;
-    let tcp_socket = TcpListener::bind(&addr)?;
+    let udp_socket = UdpSocket::bind(&addr).await?;
+    let tcp_socket = TcpListener::bind(&addr).await?;
     println!("Listening on: {}", addr);
 
-    let udp_server = UdpServer {
-        socket: udp_socket,
-        buf: vec![0; 1024],
-        to_send: None,
-    }
-    .map_err(|e| println!("Failed to handle UDP packet: {:?}", e));
+    let udp = tokio::spawn(udp_server(udp_socket));
+    let tcp = tokio::spawn(tcp_server(tcp_socket));
 
-    let tcp_server = tcp_socket
-        .incoming()
-        .map_err(|e| println!("Failed to accept connection: {:?}", e))
-        .for_each(move |socket| {
-            let server = TcpServer {
-                socket,
-                buf: vec![0; 1024],
-                to_send: None,
-            };
-
-            tokio::spawn(server.map_err(|e| println!("Failed to handle connection: {:?}", e)));
-            Ok(())
-        });
-
-    tokio::run(lazy(|| {
-        tokio::spawn(udp_server);
-        tokio::spawn(tcp_server);
-        Ok(())
-    }));
+    let server = future::join(udp, tcp);
+    let (udp_res, tcp_res) = server.await;
+    udp_res??;
+    tcp_res??;
 
     Ok(())
 }
--- dns/src/bin/tokio-server-udp.rs
+++ dns/src/bin/tokio-server-udp.rs
@@ -1,20 +1,16 @@
 use dns::build::Serialize;
 use dns::parse::parse_dns_packet;
 use dns::utils::respond;
-use futures::sink::SinkExt;
-use futures::stream::StreamExt;
 use std::net::SocketAddr;
 use std::{env, io};
-use tokio::net::{TcpListener, TcpStream};
-use tokio_util::codec::LengthDelimitedCodec;
+use tokio::net::UdpSocket;
 
-async fn tcp_handle_connection(socket: TcpStream, addr: SocketAddr) -> Result<(), io::Error> {
-    let mut framed = LengthDelimitedCodec::builder()
-        .length_field_length(2)
-        .new_framed(socket);
+async fn udp_server(mut socket: UdpSocket) -> Result<(), io::Error> {
+    let mut buf = vec![0u8; 1024];
+    loop {
+        let (nread, addr) = socket.recv_from(&mut buf).await?;
 
-    while let Some(frame) = framed.next().await {
-        let packet = match parse_dns_packet(&frame?) {
+        let packet = match parse_dns_packet(&buf[0..nread]) {
             Ok((_, packet)) => packet,
             Err(e) => {
                 eprintln!("Malformed DNS query from {:?}: {:?}", addr, e);
@@ -25,23 +21,21 @@ async fn tcp_handle_connection(socket: TcpStream, addr: SocketAddr) -> Result<()
         let mut out_buf = Vec::new();
         let resp = respond(packet, &addr);
         resp.serialize_to(&mut out_buf)?;
-        match framed.send(out_buf.into()).await {
-            Ok(()) => {
+        match socket.send_to(&out_buf, addr).await {
+            Ok(nsent) if nsent == out_buf.len() => {
                 println!("Sent response to {}", addr);
+            }
+            Ok(nsent) => {
+                eprintln!(
+                    "Failed to send whole response, sent {}/{} bytes",
+                    nsent,
+                    out_buf.len()
+                );
             }
             Err(e) => {
                 eprintln!("Failed to send response to {:?}: {:?}", addr, e);
             }
         }
-    }
-
-    Ok(())
-}
-
-async fn tcp_server(mut socket: TcpListener) -> Result<(), io::Error> {
-    loop {
-        let (conn_socket, addr) = socket.accept().await?;
-        tokio::spawn(tcp_handle_connection(conn_socket, addr));
     }
 }
 
@@ -50,9 +44,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = env::args().nth(1).unwrap_or_else(|| "[::]:35353".to_string());
     let addr = addr.parse::<SocketAddr>()?;
 
-    let tcp_socket = TcpListener::bind(&addr).await?;
+    let udp_socket = UdpSocket::bind(&addr).await?;
     println!("Listening on: {}", addr);
 
-    tcp_server(tcp_socket).await?;
+    udp_server(udp_socket).await?;
     Ok(())
 }
